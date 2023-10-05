02a471078ea6309a8b3bb43cd36b77f5cd33521b
Milosz Liput <miloszliput@gmail.com>
2023-10-05 22:25:18 +0100

    Added logic for checking if king is under check when making move

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,6 +1,7 @@
 import Square from "./Square";
 import "../styles/Board.css"
 import { useState } from "react";
+import { getAttackMoves, getMoves, getValidMoves } from "./Validator";
 
 const Board = () => {
 
@@ -9,6 +10,10 @@ const Board = () => {
     const [potentialMoves, setPotentialMoves] = useState<number[][]>([]);
     const [potentialAttacks, setPotentialAttacks] = useState<number[][]>([]);
     const [colorToMove, setColorToMove] = useState("white");
+    const [whiteKingPosition, setWhiteKingPosition] = useState([7, 4]);
+    const [blackKingPosition, setBlackKingPosition] = useState([0, 4]);
+    //const [whiteCastling, setWhiteCastling] = useState({rook70: true, rook77: true, king: true}); TODO: deal with castling after checking
+    //const [blackCastling, setBlackCastling] = useState({rook00: true, rook07: true, king: true});
     const [currentBoard, setCurrentBoard] = useState([
         ["rook_black", "knight_black", "bishop_black", "queen_black", "king_black", "bishop_black", "knight_black", "rook_black"],
         ["pawn_black", "pawn_black", "pawn_black", "pawn_black", "pawn_black", "pawn_black", "pawn_black", "pawn_black"],
@@ -20,23 +25,7 @@ const Board = () => {
         ["rook_white", "knight_white", "bishop_white", "queen_white", "king_white", "bishop_white", "knight_white", "rook_white"]
     ]);
 
-    /*const initialBoard: string[][] = [
-        ["rook_black", "knight_black", "bishop_black", "queen_black", "king_black", "bishop_black", "knight_black", "rook_black"],
-        ["pawn_black", "pawn_black", "pawn_black", "pawn_black", "pawn_black", "pawn_black", "pawn_black", "pawn_black"],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["pawn_white", "pawn_white", "pawn_white", "pawn_white", "pawn_white", "pawn_white", "pawn_white", "pawn_white"],
-        ["rook_white", "knight_white", "bishop_white", "queen_white", "king_white", "bishop_white", "knight_white", "rook_white"]
-    ];*/
-
-
-
     const makeMove = (figure: string, cordinateX: number, cordinateY: number) => {
-        const possibleMoves = getPossibleMoves(figure, cordinateX, cordinateY);
-        console.log("//" + possibleMoves);
-
         if((chosenSquareX === -1 && chosenSquareY === -1)
             || currentBoard[cordinateX][cordinateY].slice(-5) === currentBoard[chosenSquareX][chosenSquareY].slice(-5)) {
             if(currentBoard[cordinateX][cordinateY] === ""){
@@ -45,28 +34,39 @@ const Board = () => {
             if(figure !== "" && !figure.includes(colorToMove)) {
                 return;
             }
-            const attacks: number[][] = [];
-            possibleMoves.forEach(m => {
-                if(currentBoard[cordinateX + m[0]][cordinateY + m[1]] !== "" && currentBoard[cordinateX + m[0]][cordinateY + m[1]].slice(-5) !== figure.slice(-5)) {
-                    attacks.push([cordinateX + m[0], cordinateY + m[1]]);
-                }
-            })
+            
+            const kingPosition = colorToMove === "white" ? whiteKingPosition : blackKingPosition;
+            const validMoves = getValidMoves(figure, getMoves(figure, cordinateX, cordinateY, currentBoard), cordinateX, cordinateY, kingPosition, currentBoard);
+            const attacks = getAttackMoves(figure, cordinateX, cordinateY, validMoves, currentBoard);
+    
             setPotentialAttacks(attacks);
+            setPotentialMoves(validMoves);
             setChosenSquareX(cordinateX);
             setChosenSquareY(cordinateY);
-            setPotentialMoves(possibleMoves);
         } else {
             if(potentialMoves.some(m => chosenSquareX + m[0] === cordinateX && chosenSquareY + m[1] === cordinateY)) {
                 let tempBoard = currentBoard;
-                tempBoard[cordinateX][cordinateY] = tempBoard[chosenSquareX][chosenSquareY];
+                let color = currentBoard[chosenSquareX][chosenSquareY].slice(-5);
+                
+                if(currentBoard[chosenSquareX][chosenSquareY].startsWith("pawn") && (cordinateX === 0 || cordinateX === 7)) {
+                    tempBoard[cordinateX][cordinateY] = "queen_" + color; //TODO enable more choices
+                } else {
+                    tempBoard[cordinateX][cordinateY] = tempBoard[chosenSquareX][chosenSquareY];
+                }
+                if(figure === "white_king") {
+                    setWhiteKingPosition([cordinateX, cordinateY]);
+                }
+                else if(figure === "black_king") {
+                    setBlackKingPosition([cordinateX, cordinateY]);
+                }
                 tempBoard[chosenSquareX][chosenSquareY] = "";
-                setCurrentBoard(tempBoard);
+                setCurrentBoard(tempBoard);             
                 
                 if(colorToMove === "white") {
-                    setColorToMove("black");
+                    setColorToMove("black");    
                 } else {
                     setColorToMove("white");
-                }
+                }             
             }
             setPotentialAttacks([]);
             setPotentialMoves([]);
@@ -75,101 +75,6 @@ const Board = () => {
         }
     }
 
-    const getPossibleMoves = (figure: string, cordinateX: number, cordinateY: number) => {
-        const movesForFigure: number[][] = getMovesForFigure(figure, cordinateX, cordinateY);
-        
-        if(figure.startsWith("king") || figure.startsWith("knight") || figure.startsWith("pawn")) {
-            return movesForFigure.filter(m => {
-                let newX = cordinateX + m[0];
-                let newY = cordinateY + m[1];
-                if(newX < 0 || newX > 7 || newY < 0 || newY > 7) {
-                    return false;
-                }
-                if((currentBoard[newX][newY].endsWith("black") && figure.endsWith("black"))
-                    || (currentBoard[newX][newY].endsWith("white") && figure.endsWith("white"))) {
-                    return false;
-                }
-                return true;
-            });
-        }
-
-        const allMovesForFigure: number[][] = [];
-        movesForFigure.forEach(m => {
-            for(let i = 1; i < 8; i++) {
-                let newX = cordinateX + m[0] * i;
-                let newY = cordinateY + m[1] * i;
-                if(newX < 0 || newX > 7 || newY < 0 || newY > 7) {
-                    break;
-                }
-                let potentialSquare = currentBoard[newX][newY]; 
-                if(potentialSquare === "") {
-                    allMovesForFigure.push([m[0] * i, m[1] * i]);
-                } 
-                else if((potentialSquare.endsWith("white") && figure.endsWith("black")) || (potentialSquare.endsWith("black") && figure.endsWith("white"))) {
-                    allMovesForFigure.push([m[0] * i, m[1] * i]);
-                    break;
-                }
-                else {
-                    break;
-                }
-            }
-        })
-        return allMovesForFigure;
-    }
-
-    const getMovesForFigure = (figure: string, cordinateX: number, cordinateY: number) => {
-        if(figure.startsWith("pawn")) {
-            let pawnMoves: number[][] = [];
-            if(figure.endsWith("white")) {
-                if(currentBoard[cordinateX - 1][cordinateY] === "") {
-                    pawnMoves.push([-1, 0]);
-                }
-                if(cordinateX === 6 && currentBoard[5][cordinateY] === "" && currentBoard[4][cordinateY] === "") {
-                    pawnMoves.push([-2, 0]);
-                }
-                if(cordinateX > 0 && cordinateY > 0 && currentBoard[cordinateX - 1][cordinateY - 1].endsWith("black")) {
-                    pawnMoves.push([-1, -1]);
-                }
-                if(cordinateX > 0 && cordinateY < 7 && currentBoard[cordinateX + 1][cordinateY + 1].endsWith("black")) {
-                    pawnMoves.push([-1, 1])
-                }
-            } else { //black pawn
-                if(currentBoard[cordinateX + 1][cordinateY] === "") {
-                    pawnMoves.push([1, 0]);
-                }
-                if(cordinateX === 1 && currentBoard[2][cordinateY] === "" && currentBoard[3][cordinateY] === "") {
-                    pawnMoves.push([2, 0]);
-                }
-                if(cordinateX < 7 && cordinateY > 0 && currentBoard[cordinateX + 1][cordinateY - 1].endsWith("white")) {
-                    pawnMoves.push([1, -1]);
-                }
-                if(cordinateX < 7 && cordinateY < 7 && currentBoard[cordinateX + 1][cordinateY + 1].endsWith("white")) {
-                    pawnMoves.push([1, 1])
-                }
-            }
-            return pawnMoves;
-        }
-        if(figure.startsWith("rook")) {
-            //how to deal with castling
-            return [[1, 0], [-1, 0], [0, 1], [0, -1]]; 
-        }
-        if(figure.startsWith("knight")) {
-            return [[2, 1], [2, -1], [-2, 1], [-2, -1], [1, 2], [1, -2], [-1, 2], [-1, -2]];
-        }
-        if(figure.startsWith("bishop")) {
-            return [[1, 1], [1, -1], [-1, 1], [-1, -1]];
-        }
-        if(figure.startsWith("king")) {
-            // deal with castling
-            return [[1, 0], [1, 1], [1, -1], [0, 1], [0, -1], [-1, 1], [-1, 0], [-1, -1]];
-        }
-        if(figure.startsWith("queen")) {
-            return [[1, 0], [1, 1], [1, -1], [0, 1], [0, -1], [-1, 1], [-1, 0], [-1, -1]];
-        }
-        return [];
-    }
-
-    console.log(potentialAttacks);
     return (
         <div className="board">
             {
---
+++ src/components/Validator.ts
@@ -0,0 +1,147 @@
+export const getMoves = (figure: string, cordinateX: number, cordinateY: number, board: string[][]) => {
+    const movesForFigure: number[][] = getMovesForFigure(figure, cordinateX, cordinateY, board);
+
+    if(figure.startsWith("king") || figure.startsWith("knight") || figure.startsWith("pawn")) {
+        return movesForFigure.filter(m => {
+            let newX = cordinateX + m[0];
+            let newY = cordinateY + m[1];
+            if(newX < 0 || newX > 7 || newY < 0 || newY > 7) {
+                return false;
+            }
+            if((board[newX][newY].endsWith("black") && figure.endsWith("black"))
+                || (board[newX][newY].endsWith("white") && figure.endsWith("white"))) {
+                return false;
+            }
+            return true;
+        });
+    }
+
+    const allMovesForFigure: number[][] = [];
+    movesForFigure.forEach(m => {
+        for(let i = 1; i < 8; i++) {
+            let newX = cordinateX + m[0] * i;
+            let newY = cordinateY + m[1] * i;
+            if(newX < 0 || newX > 7 || newY < 0 || newY > 7) {
+                break;
+            }
+            let potentialSquare = board[newX][newY]; 
+            if(potentialSquare === "") {
+                allMovesForFigure.push([m[0] * i, m[1] * i]);
+            } 
+            else if((potentialSquare.endsWith("white") && figure.endsWith("black")) || (potentialSquare.endsWith("black") && figure.endsWith("white"))) {
+                allMovesForFigure.push([m[0] * i, m[1] * i]);
+                break;
+            }
+            else {
+                break;
+            }
+        }
+    })
+    return allMovesForFigure;
+}
+
+export const getMovesForFigure = (figure: string, cordinateX: number, cordinateY: number, board: string[][]) => {
+    if(figure.startsWith("pawn")) {
+        let pawnMoves: number[][] = [];
+        if(figure.endsWith("white")) {
+            if(board[cordinateX - 1][cordinateY] === "") {
+                pawnMoves.push([-1, 0]);
+            }
+            if(cordinateX === 6 && board[5][cordinateY] === "" && board[4][cordinateY] === "") {
+                pawnMoves.push([-2, 0]);
+            }
+            if(cordinateX > 0 && cordinateY > 0 && board[cordinateX - 1][cordinateY - 1].endsWith("black")) {
+                pawnMoves.push([-1, -1]);
+            }
+            if(cordinateX > 0 && cordinateY < 7 && board[cordinateX + 1][cordinateY + 1].endsWith("black")) {
+                pawnMoves.push([-1, 1])
+            }
+        } else { //black pawn
+            if(board[cordinateX + 1][cordinateY] === "") {
+                pawnMoves.push([1, 0]);
+            }
+            if(cordinateX === 1 && board[2][cordinateY] === "" && board[3][cordinateY] === "") {
+                pawnMoves.push([2, 0]);
+            }
+            if(cordinateX < 7 && cordinateY > 0 && board[cordinateX + 1][cordinateY - 1].endsWith("white")) {
+                pawnMoves.push([1, -1]);
+            }
+            if(cordinateX < 7 && cordinateY < 7 && board[cordinateX + 1][cordinateY + 1].endsWith("white")) {
+                pawnMoves.push([1, 1])
+            }
+        }
+        return pawnMoves;
+    }
+    if(figure.startsWith("rook")) {
+        return [[1, 0], [-1, 0], [0, 1], [0, -1]]; 
+    }
+    if(figure.startsWith("knight")) {
+        return [[2, 1], [2, -1], [-2, 1], [-2, -1], [1, 2], [1, -2], [-1, 2], [-1, -2]];
+    }
+    if(figure.startsWith("bishop")) {
+        return [[1, 1], [1, -1], [-1, 1], [-1, -1]];
+    }
+    if(figure.startsWith("king")) {
+        // deal with castling
+        return [[1, 0], [1, 1], [1, -1], [0, 1], [0, -1], [-1, 1], [-1, 0], [-1, -1]];
+    }
+    if(figure.startsWith("queen")) {
+        return [[1, 0], [1, 1], [1, -1], [0, 1], [0, -1], [-1, 1], [-1, 0], [-1, -1]];
+    }
+    return [];
+}
+
+//returns list of attack moves for a figure to show a circle around potential prey or a list of all squares under attack that king cannot go to
+export const getAttackMoves = (figure: string, fromX: number, fromY: number, possibleMoves: number[][], board: string[][], potentialAttacks: boolean = false) => {
+    const attacks: number[][] = [];
+    possibleMoves.forEach(m => {
+        if(!potentialAttacks && board[fromX + m[0]][fromY + m[1]] !== "" && board[fromX + m[0]][fromY + m[1]].slice(-5) !== figure.slice(-5)) {
+            attacks.push([fromX + m[0], fromY + m[1]]);
+        }
+        else if(potentialAttacks && board[fromX + m[0]][fromY + m[1]] === "") {
+            attacks.push([fromX + m[0], fromY + m[1]]);
+        }
+    });
+    return attacks;
+}
+
+export const getValidMoves = (figure: string, moves: number[][], fromX: number, fromY: number, kingPosition: number[], board: string[][]) => {
+    //TODO figure out hot filter moves for all pieces so they dont cause king to be checked
+    const newMoves = moves.filter(move => {
+        const newX = fromX + move[0];
+        const newY = fromY + move[1];
+
+        const tempBoard = copy2DArray(board);
+        tempBoard[newX][newY] = figure;
+        tempBoard[fromX][fromY] = "";
+
+        if(figure.startsWith("king")) {
+            return !isKingUnderCheck(newX, newY, tempBoard);
+        }
+        return !isKingUnderCheck(kingPosition[0], kingPosition[1], tempBoard);
+    });
+
+    return newMoves;
+}
+
+const isKingUnderCheck = (kingX: number, kingY: number, board: string[][]) => {
+    const color = board[kingX][kingY].slice(-5);
+
+    for(let i = 0; i < 8; i++) {
+        for(let j = 0; j < 8; j++) {
+            if(board[i][j] === "" || board[i][j].endsWith(color)) {
+                continue;
+            }
+            const moves = getMoves(board[i][j], i, j, board);
+            const attackMoves = getAttackMoves(board[i][j], i, j, moves, board);
+            if(attackMoves.some(move => kingX === move[0] && kingY === move[1])) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+const copy2DArray = (array: any[][]) => {
+    return array.map(x => [...x]);
+}
